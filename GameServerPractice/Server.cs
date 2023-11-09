8677631f39a4846667b1027594da557f01d29931
chanynotnerd <63177308+chanynotnerd@users.noreply.github.com>
2023-11-09 20:14:52 +0900

    Project Upload

---
+++ GameServerPractice/Server.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameServerPractice
+{
+    internal class Server
+    {
+        public static int MaxPlayers { get; private set; }
+
+        public static int Port { get; private set;  }
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        public delegate void PacketHandler(int _fromClient, Packet _packet);
+        public static Dictionary<int, PacketHandler> packetHandlers;
+
+        private static TcpListener tcpListener;
+
+        public static void Start(int _MaxPlayers, int _Port)
+        { 
+            MaxPlayers = _MaxPlayers;
+            Port = _Port;
+
+            Console.WriteLine($"Starting Server...");
+            InitializeServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server Started on {Port}");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i < MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 1; i < MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+
+            packetHandlers = new Dictionary<int, PacketHandler>()
+            {
+                { (int)ClientPackets.welcomeReceived, ServerHandle.WelcomeReceived }
+            };
+            Console.WriteLine("Initialized packets");
+        }
+    }
+}
